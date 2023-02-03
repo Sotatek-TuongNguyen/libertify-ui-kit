cca5a786753c06dd2a7fadde9f3a6508dfe12e87
Tuong Nguyen <tuong.nquyen@sotatek.com>
2023-02-03 18:04:16 +0700

    add multiply

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,8 @@ import Color from './Color';
 import Text from './Text';
 import Button from './Button';
 
+export function multiply(a: number, b: number): Promise<number> {
+  return Promise.resolve(a * b);
+}
+
 export { Color, Text, Button };
