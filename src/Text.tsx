40617e1dde0cfd02a1d6168c438b2b447c6b6081
Tuong Nguyen <tuong.nguyen@sotatek.com>
2023-02-03 15:23:05 +0700

    feat: update

--- src/Text.tsx
+++ src/Text.tsx
@@ -5,17 +5,7 @@ interface TextProps extends RNTextProps {
   variant?: 'h1' | 'h2' | 'h3' | 'h4';
 }
 
-export default ({ children, variant = 'h4' }: TextProps) => {
-  const getStyles = (v: string): { fontSize: number } => {
-    const variants = {
-      h1: { fontSize: 42 },
-      h2: { fontSize: 32 },
-      h3: { fontSize: 26 },
-      h4: { fontSize: 18 },
-    };
-    return variants.h4;
-  };
-  const styles = getStyles(variant);
-
+export default ({ children }: TextProps) => {
+  // eslint-disable-next-line react-native/no-inline-styles
   return <Text style={{ fontSize: 42 }}>{children}</Text>;
 };
