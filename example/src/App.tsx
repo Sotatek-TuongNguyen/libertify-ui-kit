3c7373d150db4e1f770300be30fc83f5c759e659
Tuong Nguyen <tuong.nquyen@sotatek.com>
2023-02-06 15:18:30 +0700

    text style

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 
-import { StyleSheet, View } from 'react-native';
-import { multiply, Text } from 'libertify-ui-kit';
+import { SafeAreaView, StyleSheet, View } from 'react-native';
+import { Text } from 'libertify-ui-kit';
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    multiply(3, 7).then(setResult);
-  }, []);
-
   return (
     <View style={styles.container}>
-      <Text>Result: {result}</Text>
+      <SafeAreaView>
+        <Text variant="h1">This is heading 1</Text>
+        <Text variant="h2">This is heading 2</Text>
+        <Text variant="h3">This is heading 3</Text>
+        <Text variant="h4">This is heading 4</Text>
+        <Text variant="h5">This is heading 5</Text>
+        <Text variant="subtitle1">This is subtitle 1</Text>
+        <Text variant="subtitle2">This is subtitle 2</Text>
+        <Text variant="body1">This is body 1</Text>
+        <Text variant="body2">This is body 2</Text>
+        <Text variant="body3">This is body 3</Text>
+        <Text variant="button">This is button</Text>
+        <Text variant="caption">This is caption</Text>
+        <Text variant="value1">This is value 1</Text>
+        <Text variant="value2">This is value 2</Text>
+        <Text variant="smallContent">This is small content</Text>
+        <Text variant="overline">This is overline</Text>
+      </SafeAreaView>
     </View>
   );
 }
@@ -20,8 +31,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   box: {
     width: 60,
